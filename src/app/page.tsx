89da5654884abcd05819a330151c0684f06244b6
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-10-28 08:52:28 -0600

    reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,7 @@ import Footer from "@/components/organisms/Footer";
 export default function Home() {
     return (
         <main className="">
-            <Header/>
-            <h1>Home</h1>
-            <h2>qwqw</h2>
-            <h1>bdfvnsiodfn</h1>
-            <h2>Esta es una segunda preuba</h2>
-            <p>dSgifnhtrdiozfnbiodfnviosdnviosd</p>
-            <p>eroignjtrbiomdso</p>
-            <a href="#">ewinsiov</a><p>fdnog
-                mfsDofv
-            </p>
-            <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste autem, sunt dolor dolores tempore hic architecto nostrum cumque sit nesciunt culpa alias fugiat delectus vel. Cumque quibusdam nihil laboriosam repellendus!</a>               
+            <Header/>                        
             <Footer/>
         </main>
     );
